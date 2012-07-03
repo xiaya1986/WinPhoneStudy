9f80992200dd4009c0517c67fd554abc3d05c43d
Ken Xu <xuyinjie2005@hotmail.com>
2012-07-03 17:51:02 +0800

    Add exercise in day 3

---
+++ GeoPositionWebService/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Device.Location;
+
+namespace GeoPositionWebService
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            GeoCoordinateWatcher myWatcher = new GeoCoordinateWatcher();
+            var myPosition = myWatcher.Position;
+
+            double latitude = 47.674;
+            double longitude = -122.12;
+
+            if (!myPosition.Location.IsUnknown)
+            {
+                latitude = myPosition.Location.Latitude;
+                longitude = myPosition.Location.Longitude;
+            }
+
+            myTerraService.TerraServiceSoapClient client = new myTerraService.TerraServiceSoapClient();
+            client.ConvertLonLatPtToNearestPlaceCompleted += new EventHandler<myTerraService.ConvertLonLatPtToNearestPlaceCompletedEventArgs>(client_ConvertLonLatPtToNearestPlaceCompleted);
+
+            client.ConvertLonLatPtToNearestPlaceAsync(new myTerraService.LonLatPt { Lat = latitude, Lon = longitude });
+        }
+
+        void client_ConvertLonLatPtToNearestPlaceCompleted(object sender, myTerraService.ConvertLonLatPtToNearestPlaceCompletedEventArgs e)
+        {
+            textBlock1.Text = e.Result;
+            //throw new NotImplementedException();
+        }
+    }
+}
